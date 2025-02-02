8094a2f2a329203eaab974c1f6e0f716f440e1fb
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:19:03 +0500

    code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
     authDomain: "links-sharing-b9c2a.firebaseapp.com",
     projectId: "links-sharing-b9c2a",
     storageBucket: "links-sharing-b9c2a.appspot.com",
-    messagingSenderId: "606062782904",
+    // messagingSenderId: "606062782904",
     // appId: "1:606062782904:web:c13b74d910a35e9336c74d",
     // measurementId: "G-0ZCEWD5WWJ"
 };
