071fe1ca2354fef20e5ec88524505c21cb877f0e
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:18:35 +0500

    code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
     storageBucket: "links-sharing-b9c2a.appspot.com",
     messagingSenderId: "606062782904",
     appId: "1:606062782904:web:c13b74d910a35e9336c74d",
-    measurementId: "G-0ZCEWD5WWJ"
+    // measurementId: "G-0ZCEWD5WWJ"
 };
 
 export const app = initializeApp(firebaseConfig);
