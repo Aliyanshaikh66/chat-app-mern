ce7f9dc4a23575beeedb9178f6203f4a7d7bab5d
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:16:06 +0500

    frontend-code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -6,8 +6,8 @@ const firebaseConfig = {
     projectId: "links-sharing-b9c2a",
     storageBucket: "links-sharing-b9c2a.appspot.com",
     messagingSenderId: "606062782904",
-    // appId: "1:606062782904:web:c13b74d910a35e9336c74d",
-    // measurementId: "G-0ZCEWD5WWJ"
+    appId: "1:606062782904:web:c13b74d910a35e9336c74d",
+    measurementId: "G-0ZCEWD5WWJ"
 };
 
 export const app = initializeApp(firebaseConfig);
