3bdbda4523758172739bce840449e66c8ef8e367
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:23:26 +0500

    coded

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
     projectId: "links-sharing-b9c2a",
     storageBucket: "links-sharing-b9c2a.appspot.com",
     messagingSenderId: "606062782904",
-    appId: "1:606062782904:web:c13b74d910a35e9336c74d",
+    
    
 };
 
