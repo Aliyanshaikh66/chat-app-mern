726de7ce8eada2d79395a185419961097cfb2298
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:23:58 +0500

    code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDDkH9pqIcdWmRlXHdyC2G0z0Lhlej5dCw",
     authDomain: "links-sharing-b9c2a.firebaseapp.com",
     projectId: "links-sharing-b9c2a",
-    storageBucket: "links-sharing-b9c2a.appspot.com",
+    
     
     
    
