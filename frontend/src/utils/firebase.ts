6a7dc46958accee930ed56fbfb5ebb9164f73e03
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:20:03 +0500

    code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDkH9pqIcdWmRlXHdyC2G0z0Lhlej5dCw",
+    // apiKey: "AIzaSyDDkH9pqIcdWmRlXHdyC2G0z0Lhlej5dCw",
     // authDomain: "links-sharing-b9c2a.firebaseapp.com",
     // projectId: "links-sharing-b9c2a",
     // storageBucket: "links-sharing-b9c2a.appspot.com",
