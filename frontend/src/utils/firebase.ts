9cee655dc3a7b7edeb6c556a86ee23e27d2fe587
Aliyan Shaikh <aliyanzahid02@gmail.com>
2025-02-02 22:19:21 +0500

    code

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDDkH9pqIcdWmRlXHdyC2G0z0Lhlej5dCw",
     authDomain: "links-sharing-b9c2a.firebaseapp.com",
     projectId: "links-sharing-b9c2a",
-    storageBucket: "links-sharing-b9c2a.appspot.com",
+    // storageBucket: "links-sharing-b9c2a.appspot.com",
     // messagingSenderId: "606062782904",
     // appId: "1:606062782904:web:c13b74d910a35e9336c74d",
     // measurementId: "G-0ZCEWD5WWJ"
